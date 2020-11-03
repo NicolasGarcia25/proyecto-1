45bf6a051390aa1ac1629a483843838df6a8dc7e
Nickogarcia25 <nickogarcia25@gmail.com>
2020-11-02 21:06:25 -0300

    capa js, creacion magic.html

---
+++ js/base.js
@@ -0,0 +1,182 @@
+var logo_a = document.getElementById("logo_a")
+
+logo_a.innerHTML = `<div id="img1">
+<img src="carpeta_imagenes/lithium_store_logo_transparente.png" />
+</div>
+<div style="position: relative" id="cajainput_grande">
+<div style="position: absolute; z-index: 2" id="cajainput">
+  <input id="input" type="text" />
+</div>
+<div
+  style="position: absolute; right: 18.1%; z-index: 3"
+  id="boton_busqueda"
+>
+  <img src="carpeta_imagenes/Search-button.jpg" />
+</div>
+<div
+  style="position: absolute; z-index: 1"
+  id="cajainput_transparencia"
+>
+  <div id="transparencia1"></div>
+  <div id="transparencia15"></div>
+  <div id="transparencia2"></div>
+  <div id="transparencia25"></div>
+  <div id="transparencia3"></div>
+  <div id="transparencia35"></div>
+  <div id="transparencia4"></div>
+  <div id="transparencia45"></div>
+  <div id="transparencia5"></div>
+  <div id="transparencia55"></div>
+  <div id="transparencia6"></div>
+  <div id="transparencia65"></div>
+  <div id="transparencia7"></div>
+  <div id="transparencia75"></div>
+  <div id="transparencia8"></div>
+  <div id="transparencia85"></div>
+  <div id="transparencia9"></div>
+  <div id="transparencia10"></div>
+  <div id="transparencia11"></div>
+  <div id="transparencia12"></div>
+</div>
+</div>`
+
+
+
+
+
+var menu =document.getElementById("menu")
+
+menu.innerHTML= ` <div class="item">
+<p>Magic</p>
+<p class="flecha_item">&darr;</p>
+</div>
+<div class="item">
+<p>Pokémon</p>
+<p class="flecha_item">&darr;</p>
+</div>
+<div class="item">
+<p>Yu-Gi-Oh!</p>
+<p class="flecha_item">&darr;</p>
+</div>
+<div class="item">
+<p>Vanguard</p>
+<p class="flecha_item">&darr;</p>
+</div>
+<div class="item">
+<p>Accesorios</p>
+<p class="flecha_item">&darr;</p>
+</div>
+<div class="item">
+<p>Gift Card</p>
+<p class="flecha_item">&darr;</p>
+</div>`
+
+
+
+
+
+var caja_pie =document.getElementById("caja_pie")
+
+caja_pie.innerHTML =  `<div id="divisor_pie">
+<div id="caja_texto_pie">
+  <p>Pie de pagina</p>
+</div>
+<div id="columnas_pie">
+  <div id="pie_columna1">
+    <div class="pie_columna1_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna1_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna1_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna1_item">
+      <p>asdasd</p>
+    </div>
+    <div class="pie_columna1_item">
+      <p>asdasda</p>
+    </div>
+  </div>
+
+  <div id="pie_columna2">
+    <div class="pie_columna2_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna2_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna2_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna2_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna2_item">
+      <p>asd</p>
+    </div>
+  </div>
+
+  <div id="pie_columna3">
+    <div class="pie_columna3_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna3_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna3_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna3_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna3_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna3_item">
+      <p>asd</p>
+    </div>
+  </div>
+
+  <div id="pie_columna4">
+    <div class="pie_columna4_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna4_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna4_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna4_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna4_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna4_item">
+      <p>asd</p>
+    </div>
+  </div>
+
+  <div id="pie_columna5">
+    <div class="pie_columna5_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna5_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna5_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna5_item">
+      <p>asd</p>
+    </div>
+    <div class="pie_columna5_item">
+      <p>asd</p>
+    </div>
+  </div>
+</div>
+</div>`
+
